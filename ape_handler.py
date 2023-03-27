c3364c7d28a2221dfd84383f836bccfc027e9d67
Axel Sorenson <axelps@uci.edu>
2023-03-27 04:12:24 -0700

    ape_handler module
    Added the ape_handler module, which builds a monkey type URL based off requests processed from the message_reader module which will be made next. ape_handler creates a dictionary which is then processed in the eventual ape_reader module.

---
+++ ape_handler.py
@@ -0,0 +1,28 @@
+# ape_handler.py
+# Module handles API search requests for Monkey Type.
+import json
+import urllib.parse
+import urllib.request
+
+BASE_MONKEYTYPE_URL = 'https://api.monkeytype.com/'
+
+def generate_response(APE_KEY: str, request: tuple) -> dict:
+    url = build_search_url(request[0], request[1])
+    response = get_result(url,APE_KEY)
+    return response
+
+def build_search_url(query_parameters, search_type) -> str:
+    return f'{BASE_MONKEYTYPE_URL}{search_type}?{urllib.parse.urlencode(query_parameters)}'
+
+def get_result(url: str, APE_KEY: str) -> dict:
+    response = None
+    header = {'Authorization': 'ApeKey '+APE_KEY,
+              'User-Agent': 'KeyboardKong'}
+    try:
+        request = urllib.request.Request(url, method='GET', headers = header)
+        response = urllib.request.urlopen(request)
+        json_text = response.read().decode(encoding = 'utf-8')
+        return json.loads(json_text)
+    finally:
+        if response != None:
+            response.close()
