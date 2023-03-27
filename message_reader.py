a0c21a240bedcefff88f9c4625fd7556c96be40c
Axel Sorenson <axelps@uci.edu>
2023-03-27 04:50:59 -0700

    message_reader help message
    Created a horrendous string for commands. This is temporary and only written out to formulate how commands will look in the future. Later, this will be reformatted and made more intuitive.

--- message_reader.py
+++ message_reader.py
@@ -10,12 +10,33 @@ def process_request(message: str) -> str:
         #return message
         pass
     elif message.startswith('help'):
+        #TODO: Fix this mess...
         return '```' \
                '🟡 === KeyboardKong Commands === 🟡\n' \
                '!monkey personalbest [mode] [mode2]\n' \
                'mode: Categories for personal best, such as time.\n' \
-               'mode2: Secondary category, such as 60 for time.\n' \
-               '🟡 ============================= 🟡' \
+               'mode2: Secondary category, such as 60 for time.\n\n' \
+               'This command gives the user their personal best in a given category.\n' \
+               'Make sure your discord is linked with MonkeyType!\n' \
+               '🟡 ============================= 🟡\n\n' \
+               '!monkey stats\n' \
+               'This command gives the user their typing statistics.\n' \
+               '🟡 ============================= 🟡\n\n' \
+               '!monkey profile [ID]\n' \
+               'Given a user ID, retrieve information about a user.\n' \
+               '🟡 ============================= 🟡\n\n' \
+               '!monkey leaderboard [mode] [mode2] [entry]\n' \
+               'mode: Categories for personal best, such as time.\n' \
+               'mode2: Secondary category, such as 60 for time.\n\n' \
+               'entry: how many entries to skip forward to.\n\n' \
+               'This command gives the global leaderboard for MonkeyType users.\n' \
+               '🟡 ============================= 🟡\n\n' \
+               '!monkey rank [mode] [mode2]\n' \
+               'mode: Categories for personal best, such as time.\n' \
+               'mode2: Secondary category, such as 60 for time.\n\n' \
+               'This command gives the asking user their rank.\n' \
+               '🟡 ============================= 🟡\n' \
+               '' \
                '```'
     else:
         return None
